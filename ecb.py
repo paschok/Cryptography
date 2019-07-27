e044f279890656f6974142636c4e8dcd02471fe1
Paul Mospan <pavel.mospan@yahoo.com>
2019-07-27 21:25:42 +0300

    PM and anti-PM is ready

--- ecb.py
+++ ecb.py
@@ -1,10 +1,19 @@
 """
-    ECB blockcipher. The English alphabet, the number of letters is 26.
+    ECB block cipher. The English alphabet, the number of letters is 26.
     The text is encoded using the permutation matrix PM and a the INTEGER number of divisions, that evenly divide
     the text unto blocks M-i
 
+    I define inputted text with the help of Unicode and Unicode will continue helping me, but this time doing even more,
+    than before. Each word will be transferred into INT number according to Unicode, and than each IINt will be represented
+    as bytes. Our range of possible numbers is as follows:
+    97 (decimal)  = 0110 0001 (binary)
+    122 (decimal) = 0111 1010 (binary)
+
+    Based on this our length is: N = 8
+
     ENCODING: Ek(Mi) = Ci ==> Ci = PM x Mi
     DECODING: Dk(Ci) = Mi ==> Mi = PM^(-1) x Ci
+
 
     klartext (de) = plain text
     schluesseltext (de) = encoded text
@@ -13,11 +22,38 @@
 """
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
+N = 8
 schluesseltext = ''
 
 wrong_key = True
 wrong_text = True
+
+# Permutation Matrix. 'Handmade' so that we could see what she actually is
+PM = [
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0],
+    ]
+
+# PM^-1 is needed for decoding and looks like this:
+PM_1 = [
+        [0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0],
+    ]
+print(PM_1)
+
+
 
 
 def check_integer(key_check):
@@ -64,7 +100,9 @@ while wrong_key:
 
 
 if not wrong_text and not wrong_key:
+    key = int(key)
     print(3)
+    print(type(key))
 else:
     print('Goodbye. Hope you have had fun!')
 
